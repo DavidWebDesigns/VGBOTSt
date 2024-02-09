de6818d6a312e549431149a9b6508d14802a14f4
davidwebdesigns <daeperezh@gmail.com>
2024-02-08 19:41:33 -0500

    This is my first test

---
+++ my-vg-chatbot/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import ChatBotBase from './components/ChatBotBase.vue'
+</script>
+
+<template>
+  <ChatBotBase/>
+</template>
+
+<style scoped>
+/* the style is in my soul */
+</style>
---
+++ my-vg-chatbot/src/components/ChatBotBase.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template class="iframe">
+  <div class="chat_container">
+    <iframe src="https://app.stammer.ai/es/chatbot/embed/b4bfbbfe-5447-4bd9-9d80-c0bc2e74404f" width="100%" height="100vh" frameborder="0" ></iframe>
+  </div>
+
+</template>
+
+<style scoped>
+.chat_container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 99vh;
+  width: 99vw;
+}
+iframe{
+  height: 98vh;
+  width: 99vw;
+}
+</style>
